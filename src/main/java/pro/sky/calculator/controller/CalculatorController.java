561291338f7dba4755b0928f2df249cfce1d7da2
Homework3 <tradervdovin@gmail.com>
2023-03-19 14:01:22 +0500

    Calculator

--- src/main/java/pro/sky/calculator/controller/CalculatorController.java
+++ src/main/java/pro/sky/calculator/controller/CalculatorController.java
@@ -3,6 +3,7 @@ package pro.sky.calculator.controller;
 
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.awt.*;
@@ -10,9 +11,30 @@ import java.awt.*;
 @RestController
 public class CalculatorController {
 
-    @GetMapping(produces = MediaType.TEXT_HTML_VALUE)
+    @GetMapping(path = "/calculator")
     public String welcome(){
-        return "<h1> Welcome to calculator! </h1>";
+        return "Welcome to calculator";
 
+    }
+
+
+    @GetMapping("/calculator/plus")
+    public String plus(@RequestParam("num1") int a, @RequestParam("num2") int b) {
+    return a+ " + " +b+ " = " +(a+b);
+    }
+
+    @GetMapping("/calculator/minus")
+    public String minus(@RequestParam("num1") byte a, @RequestParam("num2") byte b) {
+        return a+ " - " +b+ " = " +(a-b);
+    }
+
+    @GetMapping("/calculator/multiply")
+    public String multiply(@RequestParam("num1") short a, @RequestParam("num2") short b) {
+        return a+ " * " +b+ " = " +(a * b);
+    }
+
+    @GetMapping("/calculator/divide")
+    public String divide(@RequestParam("num1") long a, @RequestParam("num2") long b) {
+        return a+ " / " +b+ " = " +(a / b);
     }
 }
