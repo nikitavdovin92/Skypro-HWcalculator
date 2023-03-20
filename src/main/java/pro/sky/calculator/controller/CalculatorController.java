1a96b0a41e548ba2b72613a667a90f9d503c8292
Homework3 <tradervdovin@gmail.com>
2023-03-20 21:44:42 +0500

    Calculator 2

--- src/main/java/pro/sky/calculator/controller/CalculatorController.java
+++ src/main/java/pro/sky/calculator/controller/CalculatorController.java
@@ -3,38 +3,62 @@ package pro.sky.calculator.controller;
 
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import pro.sky.calculator.service.CalculatorService;
 
 import java.awt.*;
 
 @RestController
+
+@RequestMapping("/calculator")
 public class CalculatorController {
 
-    @GetMapping(path = "/calculator")
+    private final CalculatorService calculatorService;
+
+    public CalculatorController (CalculatorService calculatorService) {
+        this.calculatorService = calculatorService;
+    }
+
+    @GetMapping
     public String welcome(){
         return "Welcome to calculator";
 
     }
 
 
-    @GetMapping("/calculator/plus")
-    public String plus(@RequestParam("num1") Integer a, @RequestParam("num2") Integer b) {
-    return a+ " + " +b+ " = " +(a+b);
+    @GetMapping("/plus")
+    public String plus(@RequestParam(value = "num1", required = false) Integer a,
+                       @RequestParam(value = "num2", required = false) Integer b) {
+    return buildView(a,b, calculatorService.plus(a,b), "+");
     }
 
-    @GetMapping("/calculator/minus")
-    public String minus(@RequestParam("num1") Integer a, @RequestParam("num2") Integer b) {
-        return a+ " - " +b+ " = " +(a-b);
+    @GetMapping("/minus")
+    public String minus(@RequestParam(value = "num1", required = false) Integer a,
+                       @RequestParam(value = "num2", required = false) Integer b) {
+        return buildView(a,b, calculatorService.minus(a,b), "-");
     }
 
-    @GetMapping("/calculator/multiply")
-    public String multiply(@RequestParam("num1") Integer a, @RequestParam("num2") Integer b) {
-        return a+ " * " +b+ " = " +(a * b);
+    @GetMapping("/multiply")
+    public String multiply(@RequestParam(value = "num1", required = false) Integer a,
+                       @RequestParam(value = "num2", required = false) Integer b) {
+        return buildView(a,b, calculatorService.multiply(a,b), "*");
     }
 
-    @GetMapping("/calculator/divide")
-    public String divide(@RequestParam("num1") Integer a, @RequestParam("num2") Integer b) {
-        return a+ " / " +b+ " = " +(a / b);
+    @GetMapping("/divide")
+    public String divide(@RequestParam(value = "num1", required = false) Integer a,
+                       @RequestParam(value = "num2", required = false) Integer b) {
+        return buildView(a,b, calculatorService.divide(a,b), "/");
+    }
+
+    private String buildView(Integer a, Integer b, Number result, String operation) {
+        if (a == null || b == null) {
+            return "Не передан один из параметров";
+        }
+        if ("/".equals(operation) && b == 0) {
+            return "На ноль делить нельзя";
+        }
+        return a + " " + operation + " " + b + " = " + result;
     }
 }
